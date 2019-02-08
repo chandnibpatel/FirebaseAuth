7493deac4421b685b9282d9c8ee418307f226102
chandni <chandnibpatel@gmail.com>
2019-02-08 14:22:30 -0600

    test

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -11,24 +11,7 @@ var config = {
 
 
 var provider = new firebase.auth.GoogleAuthProvider();
-
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+toggleSignIn();
 
 // firebase.auth().signInWithRedirect(provider);
 
@@ -50,6 +33,74 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
 //     var credential = error.credential;
 //     // ...
 //   });
+
+  function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+      // [START createprovider]
+      var provider = new firebase.auth.GoogleAuthProvider();
+      // [END createprovider]
+      // [START addscopes]
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
+      // [END addscopes]
+      // [START signin]
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-oauthtoken').textContent = token;
+        // [END_EXCLUDE]
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential; 
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert("You have already signed up with a different auth provider for that email.");
+          // If you are using multiple auth providers on your app you should handle linking
+          // the user's accounts here.
+        }
+    else if (errorCode === 'auth/auth-domain-config-required') {
+      alert("An auth domain configuration is required"); 
+        }
+        else if (errorCode === 'auth/cancelled-popup-request') {
+            alert("Popup Google sign in was canceled");
+        }
+        else if (errorCode === 'auth/operation-not-allowed') {
+            alert("Operation is not allowed");
+        }
+        else if (errorCode === 'auth/operation-not-supported-in-this-environment') {
+            alert("Operation is not supported in this environment");
+        }
+        else if (errorCode === 'auth/popup-blocked') {
+            alert("Sign in popup got blocked");
+        }
+        else if (errorCode === 'auth/popup-closed-by-user') {
+            alert("Google sign in popup got cancelled");
+        }
+        else if (errorCode === 'auth/unauthorized-domain') {
+            alert("Unauthorized domain");
+        }
+         else {
+          console.error(error);
+        }
+        // [END_EXCLUDE]
+      });
+      // [END signin]
+    } else {
+      // [START signout]
+      firebase.auth().signOut();
+      // [END signout]
+    }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-ing').disabled = false;
+    // [END_EXCLUDE]
+  }
 
   firebase.auth().signOut().then(function() {
     // Sign-out successful.
